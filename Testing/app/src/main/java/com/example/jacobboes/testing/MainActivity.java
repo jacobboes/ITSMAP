79feb3d3e6b5e6109eadde254b1172086b4297eb
jacobboes <jacobboeseriksen@hotmail.com>
2017-03-29 22:11:23 +0200

    Second

--- Testing/app/src/main/java/com/example/jacobboes/testing/MainActivity.java
+++ Testing/app/src/main/java/com/example/jacobboes/testing/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jacobboes.testing;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
     }
 }
