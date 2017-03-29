99676aca6cfe78f01695112befb7a3cc61e10869
jacobboes <jacobboeseriksen@hotmail.com>
2017-03-29 22:37:33 +0200

    Creating intent

--- Testing/app/src/main/java/com/example/jacobboes/testing/DisplayMessageActivity.java
+++ Testing/app/src/main/java/com/example/jacobboes/testing/DisplayMessageActivity.java
@@ -1,4 +1,21 @@
 package com.example.jacobboes.testing;
 
-class DisplayMessageActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.jacobboes.testing.MainActivity.EXTRA_MESSAGE;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(intent.getStringExtra(EXTRA_MESSAGE));
+    }
 }
