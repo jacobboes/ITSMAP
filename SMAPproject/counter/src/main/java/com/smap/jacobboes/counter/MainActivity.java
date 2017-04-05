e20ef65c628c4e3099c364feff8ceb7783d61c96
jacobboes <jacobboeseriksen@hotmail.com>
2017-04-05 21:34:52 +0200

    counter

--- SMAPproject/counter/src/main/java/com/smap/jacobboes/counter/MainActivity.java
+++ SMAPproject/counter/src/main/java/com/smap/jacobboes/counter/MainActivity.java
@@ -2,30 +2,43 @@ package com.smap.jacobboes.counter;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String COUNTING_INT = "counting_int";
+
+    private int counter;
+    private TextView countingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        countingText = (TextView) findViewById(R.id.countingText);
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNTING_INT);
+            countingText.setText(Integer.toString(counter));
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        fab.setOnClickListener(view -> {
+            counter++;
+            countingText.setText(Integer.toString(counter));
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(COUNTING_INT, counter);
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
@@ -43,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_exit) {
+            finish();
             return true;
         }
 
